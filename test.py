4ad02d27f3bc2de33e716904412b346cd2ae5dc6
reirei0126 <reipro126@gmail.com>
2020-12-14 21:08:16 +0900

    96fps

--- test.py
+++ test.py
@@ -2,52 +2,68 @@ import numpy as np
 from bvh import Bvh
 import math
 import matplotlib.pyplot as plt
+import csv
 
-with open('lock/lock_wide.bvh','r') as f:
-    mocap=Bvh(f.read())
-    joints_name = mocap.get_joints_names()
-    borns_length=list()
-    joint_position_x=list()
-    joint_position_y=list()
-    joint_position_z=list()
+with open('take001_lock_chr00.bvh','r') as f:
+    mocap = Bvh(f.read())
+    
     frame_num=list(range(mocap.nframes))
-    for item in joints_name:
-        if item=='Hips':
-            borns_length.append(0)
-           # print("Hips")
-        else:
-            #mocap.joint_parent(item).name
-            #print(mocap.joint_offset(item)[0])
-            a=(mocap.joint_offset(item)[0])**2+(mocap.joint_offset(item)[1])**2+(mocap.joint_offset(item)[2])**2
-            borns_length.append(math.sqrt(a))
-    item='Hips'
+
+    joint_position_x = list()
+    joint_position_y = list()
+    joint_position_z = list()
+    
+    item='RightArm'
     print(item)
     for frame in range(0,mocap.nframes):#0,mocap.nframes
-        joint_position_x.append(mocap.frame_joint_channel(frame, item, 'Xrotation'))
-        joint_position_y.append(mocap.frame_joint_channel(frame, item, 'Yrotation'))
-        joint_position_z.append(mocap.frame_joint_channel(frame, item, 'Zrotation'))
-        # joint_position.append([
-        #             mocap.frame_joint_channel(frame, item, 'Xrotation'),
-        #             mocap.frame_joint_channel(frame, item, 'Yrotation'),
-        #             mocap.frame_joint_channel(frame, item, 'Zrotation')
-        #             # mocap.frame_joint_channel(frame, item, 'Xposition'),
-        #             # mocap.frame_joint_channel(frame, item, 'Yposition'),
-        #             # mocap.frame_joint_channel(frame, item, 'Zposition')
-        #         ])
-    # for item in joints_name:
-    #     if item=='Hips':
-    #         joint_position.append([
-    #             mocap.frame_joint_channel(0, item, 'Xposition'),
-    #             mocap.frame_joint_channel(0, item, 'Yposition'),
-    #             mocap.frame_joint_channel(0, item, 'Zposition')
-    #         ])
-    #     else:
-    #         joint_position.append([
-    #                 (mocap.frame_joint_channel(0, item, 'Xrotation')),
-    #                 mocap.frame_joint_channel(0, item, 'Yrotation'),
-    #                 mocap.frame_joint_channel(0, item, 'Zrotation')
-    #                 ])
+        #join_position_x
+        
+        if frame==0:
+            joint_position_x.append(mocap.frame_joint_channel(frame, item, 'Xrotation'))
+        else:
+            if (mocap.frame_joint_channel(frame, item, 'Xrotation')-joint_position_x[frame-1])>70:
+                joint_position_x.append(180-mocap.frame_joint_channel(frame, item, 'Xrotation'))    
+            elif (mocap.frame_joint_channel(frame, item, 'Xrotation')-joint_position_x[frame-1])<-70:
+                joint_position_x.append(180+mocap.frame_joint_channel(frame, item, 'Xrotation'))
+            else:
+                joint_position_x.append(mocap.frame_joint_channel(frame, item, 'Xrotation'))
+        
+        if frame==0:
+            joint_position_y.append(mocap.frame_joint_channel(frame, item, 'Yrotation'))
+        else:
+            if (mocap.frame_joint_channel(frame, item, 'Yrotation')-joint_position_y[frame-1])>90:
+                joint_position_y.append(180-mocap.frame_joint_channel(frame, item, 'Yrotation'))    
+            elif (mocap.frame_joint_channel(frame, item, 'Yrotation')-joint_position_y[frame-1])<-90:
+                joint_position_y.append(180+mocap.frame_joint_channel(frame, item, 'Yrotation'))
+            else:
+                joint_position_y.append(mocap.frame_joint_channel(frame, item, 'Yrotation'))
+        
+        if frame==0:
+            joint_position_z.append(mocap.frame_joint_channel(frame, item, 'Zrotation'))
+        else:
+            if (mocap.frame_joint_channel(frame, item, 'Zrotation')-joint_position_z[frame-1])>90:
+                joint_position_z.append(180-mocap.frame_joint_channel(frame, item, 'Zrotation'))    
+            elif (mocap.frame_joint_channel(frame, item, 'Zrotation')-joint_position_z[frame-1])<-90:
+                joint_position_z.append(180+mocap.frame_joint_channel(frame, item, 'Zrotation'))
+            else:
+                joint_position_z.append(mocap.frame_joint_channel(frame, item, 'Zrotation'))
+    
+
+        
     print("hahaha")
+
+file = open('new.csv', 'w',newline="")    #既存でないファイル名を作成してください
+w = csv.writer(file)
+
+for frame in range(0,mocap.nframes):
+    w.writerow([
+        frame,joint_position_x[frame],mocap.frame_joint_channel(frame,item,'Xrotation'),
+        joint_position_y[frame],mocap.frame_joint_channel(frame,item,'Yrotation'),
+        joint_position_z[frame],mocap.frame_joint_channel(frame,item,'Zrotation'),])
+
+ 
+file.close()
+
 fig = plt.figure()
 
 ax = fig.add_subplot(3, 1, 1)
@@ -59,4 +75,10 @@ ay.plot(frame_num, joint_position_y, marker="v", color = "blue", linestyle = ":"
 az = fig.add_subplot(3, 1, 3)
 az.plot(frame_num, joint_position_z, marker="v", color = "green", linestyle = ":")
 #plt.savefig("hogez.png")
-plt.savefig("Hips.png")
+'''
+fig = plt.figure()
+
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(frame_num, joint_position_x, marker="o", color = "red", linestyle = "--")
+'''
+plt.savefig("filter2.png")
